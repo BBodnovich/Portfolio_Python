95cadb6eac478f38e545dd2d0b229d572aa8fde0
DannSanchez <Bodnovich.BT@Gmail.com>
2024-04-03 09:36:33 -0400

    Optimizations for price_check_macbook.py

--- price_check_macbook.py
+++ price_check_macbook.py
@@ -1,24 +1,25 @@
 '''
-Purpose:
-Scan the BestBuy website daily to check the current price of the Macbook Pro M3 Max
-If the price is under the specified value you'll receive an email with the information
-Information includes product title, current price, and link to the product sale page
+Scan BestBuy website daily to check current price of the Macbook Pro M3 Max
+If price is under the target value an email notification is sent
+Information includes product title, current price, and link to the product web page
 
 Usage:
 1) Upload code to https://www.pythonanywhere.com
-2) Edit connection variables my_email, my_token, target_email
-   Recommend using EXPORT instead of hard coding token
+2) Export environment variables for MY_EMAIL, MY_TOKEN, and TARGET_EMAIL
 3) Modify smtplib address if not using a gmail account
 3) Modify variable target_price if necessary
 4) Run the code and wait for your price drop email
 '''
 
-
+import os
 import smtplib
 import time
 import requests
 from bs4 import BeautifulSoup
 
+MY_EMAIL = os.environ.get("MY_EMAIL_ADDRESS")
+MY_TOKEN = os.environ.get("MY_EMAIL_TOKEN")
+TARGET_EMAIL = os.environ.get("TARGET_EMAIL_ADDRESS")
 
 agent = {"User-Agent":"Mozilla/5.0"}
 web_url = "https://www.bestbuy.com/site/apple-macbook-pro-14-laptop-m3-max-chip-36gb-memory-30-core-gpu-1tb-ssd-latest-model-silver/6534621.p?skuId=6534621"
@@ -38,15 +39,12 @@ def price_check():
     target_price = 3000
     current_price = float(price.replace('$', '').replace(',', ''))
     if current_price < target_price:
-        my_email = "SOURCE_EMAIL_ADDRESS"
-        my_token = "YOUR_EMAIL_TOKEN"
-        target_email = "TARGET_EMAIL_ADDRESS"
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
-            connection.login(user=my_email, password=my_token)
+            connection.login(user=MY_EMAIL, password=MY_TOKEN)
             connection.sendmail(
-                from_addr=my_email, 
-                to_addrs=target_email,
+                from_addr=MY_EMAIL,
+                to_addrs=TARGET_EMAIL,
                 msg="Subject:MacBook Pro on sale!\n\n"
                 f"Item:\n{str(title)}\n"
                 f"\nPrice:\n{price}\n"
