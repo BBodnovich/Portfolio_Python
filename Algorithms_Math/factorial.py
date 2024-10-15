95b283421f025596e644b931c4ff90996d9b37d8
BBodnovich <Bodnovich.BT@Gmail.com>
2024-10-15 08:43:48 -0400

    Added comments and input validation

--- Algorithms_Math/factorial.py
+++ Algorithms_Math/factorial.py
@@ -1,8 +1,25 @@
 def find_factorial(number):
+    """
+    Calculate the factorial of a non-negative integer.
+
+    Parameters:
+    number (int): A non-negative integer
+
+    Returns:
+    int: Factorial of given number
+    """
+
+    # Input Validation Checks
     if number < 0:
         raise ValueError("Negative numbers not supported")
+
+    if not isinstance(number, int):
+        raise TypeError("Input must be an integer")
+
+    # Factorial Algorithm
     if number == 0:
         return 1
+
     answer = 1
     for i in range(2, number + 1):
         answer *= i
